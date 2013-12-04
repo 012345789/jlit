4de6236495fc825b46eedd29587cb86e25aa0992
Gorlan Yuen <holyduper@gmail.com>
2013-12-04 04:06:45 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jlit::Application.config.secret_token = '6ed7ff9421d1eb4db5124eb16fd605e7ace6626114bb895a61c6afad75b2551c6540f23f9ffa6a664d6d0a4884df5a66a0466b01a61cb909068aacb971715577'
